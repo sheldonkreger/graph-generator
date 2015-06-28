127d73c469f42e16c56365c6762e680ed918da02
Sheldon Kreger <sheldonkreger@gmail.com>
2015-06-27 19:24:32 -0700

    Add functions to create pages and users csv files.

--- graph-generator.php
+++ graph-generator.php
@@ -1,15 +1,16 @@
 <?php
 
-function generate_connections ($users, $nodes, $max_views_per_user, $conncections_file_path) {
+function generate_connections ($users, $nodes, $max_views_per_user, $connections_file_path) {
 
-	if (!$file = fopen($output_file_path, 'w')) {
+	if (!$file = fopen($connections_file_path, 'w')) {
 		print_r('Unable to open file to create connections CSV.');
 	}
 
 	$i = 0;
 	while ($i < $users) {
+		$i++;
 		$j = 1;
-		$rand = rand(0, $max_views);
+		$rand = rand(0, $max_views_per_user);
 		while ($j <= $rand) {
                         $output = array();
 			$output[] = $i;
@@ -17,7 +18,34 @@ function generate_connections ($users, $nodes, $max_views_per_user, $conncection
 			fputcsv($file, $output);
 			$j++;
 		}
+	}
+	fclose($file);
+}
+
+function generate_users ($users, $users_file_path) {
+	$i = 0;
+	if (!$file = fopen($users_file_path, 'w')) {
+               print_r('Unable to open file to create users CSV.');
+	}
+	while ($i < $users) {
 		$i++;
+		$output = array();
+		$output[] = $i;
+		fputcsv($file, $output);
+	}
+	fclose($file);
+}
+
+function generate_pages ($pages, $pages_file_path) {
+	if (!$file = fopen($pages_file_path, 'w')) {
+		print_r('Unable to open file to create pages CSV.');
+	}
+	$i = 0;
+	while ($i < $pages) {
+		$i++;
+		$output = array();
+		$output[] = $i;
+		fputcsv($file, $output);
 	}
 	fclose($file);
 }
@@ -31,14 +59,19 @@ $pages = 100;
 
 // Maximum unique page views per user.
 // We don't count hitting the same page multiple times.
-$max_views = 5;
+$max_views_per_user = 5;
 
 // We will use this as a property value.
 $base_url = 'sheldonkreger.com';
 
-// Edit to fit your filesystem.
-$connections_file_path = '~/connections.csv';
+// This is where outpt CSV files will go. Edit to fit your filesystem.
+$connections_file_path = '/home/sheldon/connections.csv';
+$pages_file_path = '/home/sheldon/pages.csv';
+$users_file_path= '/home/sheldon/users.csv';
 
-// Invoke the function.
+// Invoke the functions.
+generate_pages($pages, $pages_file_path);
+generate_users($users, $users_file_path);
 generate_connections($users, $pages, $max_views_per_user, $connections_file_path);
+
 
