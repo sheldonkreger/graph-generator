3b55338be7d82c8e797884ec290bd3859655a25e
Sheldon Kreger <sheldonkreger@gmail.com>
2015-06-27 17:40:33 -0700

    Define output file as variable.

--- graph-generator.php
+++ graph-generator.php
@@ -1,8 +1,8 @@
 <?php
 
-function generate_connections ($users, $nodes, $max_views, $base_url) {
+function generate_connections ($users, $nodes, $max_views, $base_url, $output_file_path) {
 
-	if (!$file = fopen('/home/sheldon/connections.txt', 'w')) {
+	if (!$file = fopen($output_file_path, 'w')) {
 		print_r('Unable to open file.');
 	}
 
@@ -22,13 +22,25 @@ function generate_connections ($users, $nodes, $max_views, $base_url) {
 		$i++;
 	}
 	fclose($file);
-	print_r('closed file.');
 }
 
+// The number of users to be represented in your graph.
 $users = 10;
+
+// The number of pages your users could visit. On a Drupal site, each page is represented intenally as 'www.blah.com/node/int_value'.
+// I follow this convention here. Drupal just happens to call them 'nodes'. :-)
 $nodes = 100;
+
+// Maximum unique page views per user.
+// We don't count hitting the same page multiple times.
 $max_views = 5;
+
+// We will use this as a property value.
 $base_url = 'sheldonkreger.com';
 
-generate_connections($users, $nodes, $max_views, $base_url);
+// Edit to fit your filesystem.
+$output_file_path = '/home/sheldon/connections.csv';
+
+// Invoke the function.
+generate_connections($users, $nodes, $max_views, $base_url, $output_file_path);
 
