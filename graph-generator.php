be604aa58869d77fb20da9694c76486000122d70
Sheldon Kreger <sheldonkreger@gmail.com>
2015-06-27 19:00:15 -0700

    Clarify var names.

--- graph-generator.php
+++ graph-generator.php
@@ -1,9 +1,9 @@
 <?php
 
-function generate_connections ($users, $nodes, $max_views, $base_url, $output_file_path) {
+function generate_connections ($users, $nodes, $max_views_per_user, $conncections_file_path) {
 
 	if (!$file = fopen($output_file_path, 'w')) {
-		print_r('Unable to open file.');
+		print_r('Unable to open file to create connections CSV.');
 	}
 
 	$i = 0;
@@ -12,7 +12,6 @@ function generate_connections ($users, $nodes, $max_views, $base_url, $output_fi
 		$rand = rand(0, $max_views);
 		while ($j <= $rand) {
                         $output = array();
-			$output[] = $base_url;
 			$output[] = $i;
 			$output[] = rand(0, $nodes);
 			fputcsv($file, $output);
@@ -28,7 +27,7 @@ $users = 10;
 
 // The number of pages your users could visit. On a Drupal site, each page is represented intenally as 'www.blah.com/node/int_value'.
 // I follow this convention here. Drupal just happens to call them 'nodes'. :-)
-$nodes = 100;
+$pages = 100;
 
 // Maximum unique page views per user.
 // We don't count hitting the same page multiple times.
@@ -38,8 +37,8 @@ $max_views = 5;
 $base_url = 'sheldonkreger.com';
 
 // Edit to fit your filesystem.
-$output_file_path = '/home/sheldon/connections.csv';
+$connections_file_path = '~/connections.csv';
 
 // Invoke the function.
-generate_connections($users, $nodes, $max_views, $base_url, $output_file_path);
+generate_connections($users, $pages, $max_views_per_user, $connections_file_path);
 
